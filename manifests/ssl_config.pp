13630758705dba96615583e144c85394edc96c33
o <o@immerda.ch>
2015-01-05 17:55:32 +0100

    lets cuddle with some old outlook clients

--- manifests/ssl_config.pp
+++ manifests/ssl_config.pp
@@ -20,18 +20,22 @@ class certs::ssl_config(
   $tls10_pfs_ec  = "ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA"
   $tls10_pfs_log = "DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-DSS-AES256-SHA:DHE-DSS-AES128-SHA"
   $other_pfs     = "kEDH+AES"
-  $legacy_aes    = "AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256"
+  $legacy        = "AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256"
+  $legacy_sha1   = "AES256-SHA:AES128-SHA"
   $excludes      = "!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS"
   $exclude_proto = "!SSLv2"
 
   if ($base_cipher_override != absent) {
-    $base_ciphers = $base_cipher_override
+    $base_ciphers_http = $base_cipher_override
+    $base_ciphers      = $base_cipher_override
   } else {
-    $base_ciphers = "${pfs_ae_log}:${pfs_ae_ec}:${other_pfs_ae}:${pfs_log}:${pfs_ec}:${tls10_pfs_ec}:${tls10_pfs_log}:${other_pfs}:${legacy_aes}"
+    $base_ciphers_http = "${pfs_ae_log}:${pfs_ae_ec}:${other_pfs_ae}:${pfs_log}:${pfs_ec}:${tls10_pfs_ec}:${tls10_pfs_log}:${other_pfs}:${legacy}"
+    $base_ciphers      = "${base_ciphers_http}:${legacy_sha1}"
   }
 
-  $ciphers_tls12         = "${pfs_ae_log}:${pfs_ae_ec}:${excludes}:${exclude_proto}:!SSLv3"
-  $ciphers               = "${base_ciphers}:${excludes}:${exclude_proto}"
+  $ciphers_tls12       = "${pfs_ae_log}:${pfs_ae_ec}:${excludes}:${exclude_proto}:!SSLv3"
+  $ciphers_http        = "${base_ciphers_http}:${excludes}:${exclude_proto}"
+  $ciphers             = "${base_ciphers}:${excludes}:${exclude_proto}"
 
   # Opportunistic cipher selection e.g. for smtp s2s communication, where we soft-fail anyways
   $opportunistic_ciphers = "ALL:!aNULL:!eNULL:@STRENGTH"
